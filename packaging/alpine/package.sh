34ef64130e0c903f70110bc194639aec69a6acab
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2024-08-06 14:52:50 +0200

    Bisect Alpine permission denied problems.

--- packaging/alpine/package.sh
+++ packaging/alpine/package.sh
@@ -6,7 +6,6 @@ apk update
 apk add alpine-sdk
 adduser $BUILDUSER -D
 adduser $BUILDUSER abuild
-chown -R $BUILDUSER:abuild /stoney
 if [ -d /stoney/keys ]; then
   cp -r /stoney/keys /home/$BUILDUSER/.abuild
 else
@@ -14,7 +13,6 @@ else
 fi
 cp -r /stoney/pkg /home/$BUILDUSER/packages 2>/dev/null || :
 chown -R $BUILDUSER:$BUILDUSER /home/$BUILDUSER/.abuild
-chown -R $BUILDUSER:$BUILDUSER /home/$BUILDUSER/packages
 cp /home/$BUILDUSER/.abuild/*.pub /etc/apk/keys
 chown -R $BUILDUSER:$BUILDUSER /home/$BUILDUSER
 chown -R $BUILDUSER:$BUILDUSER /stoney
@@ -22,6 +20,5 @@ chown -R $BUILDUSER:$BUILDUSER /builds
 su $BUILDUSER -c "cd /stoney/pkg/community/linux-chrultrabook-stoney
 abuild checksum
 abuild -rK"
-find /stoney -name '*.apk' | xargs -I '{}' mv "{}" /builds/
-cp -r /home/$BUILDUSER/packages /builds/pkg
+cp -r /home/$BUILDUSER/packages/community/x86_64 /builds/pkg
 cp -r /home/$BUILDUSER/.abuild /builds/keys
